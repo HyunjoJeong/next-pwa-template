d74eb7430d8ac17826f94f0f7b2603c08ad69189
HyunjoJeong <twinkleandy@snu.ac.kr>
2023-12-18 22:28:03 +0900

    ✨ feat: pwa 설정 1차

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,21 @@
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import type { ReactNode } from 'react';
 import './globals.css';
 
 export const metadata: Metadata = {
   title: 'Next PWA APP',
   description: 'PWA APP with Nextjs(v14)',
+  manifest: '/manifest.json',
+  icons: {
+    apple: '/icon-192x192.png',
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: 'white' },
+    { media: '(prefers-color-scheme: dark)', color: 'black' },
+  ],
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
